ba6e3e51d2a438def21e12e851363ffc2ee007a1
irinachurko <87023017+irinachurko@users.noreply.github.com>
2021-09-14 18:11:16 -0300

    Se agrega test de PromoAbsoluta

--- test/sistemaTurismo/PromoAbsolutaTest.java
+++ test/sistemaTurismo/PromoAbsolutaTest.java
@@ -2,13 +2,25 @@ package sistemaTurismo;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class PromoAbsolutaTest {
 
+	public List<Facturable> atraccion = new ArrayList<Facturable>();
+	
+	PromoAbsoluta promoAbs = new PromoAbsoluta(atraccion, TipoDeAtraccion.DEGUSTACION, "Promocion Absoluta", 11);
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void esPromocionTest() {
+		assertTrue(promoAbs.esPromocion());
+	}
+	
+	@Test
+	public void obtenerCostoTotalTest() {
+		assertEquals(11,promoAbs.obtenerCostoTotal(), 0);	
 	}
 
 }
