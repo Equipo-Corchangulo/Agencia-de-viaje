7582c0da292f39b593b532be7a54d1f99f334a93
Micaela Miranda <87485117+MicMiranda@users.noreply.github.com>
2021-09-14 20:08:38 -0300

    Update Atraccion.java

--- src/sistemaTurismo/Atraccion.java
+++ src/sistemaTurismo/Atraccion.java
@@ -24,8 +24,8 @@ public class Atraccion implements Facturable {
 
 	@Override
 	public String toString() {
-		return "Atracción: " + nombre + ", duración: " + tiempoPromedio + ", costo: " + costoVisita +
-				" tipo de atracción: " + tipoAtraccion  + "\n";
+		return "Atraccion: " + nombre + ", duracion: " + tiempoPromedio + ", costo: " + costoVisita +
+				" tipo de atraccion: " + tipoAtraccion  + "\n";
 
 	}
 
